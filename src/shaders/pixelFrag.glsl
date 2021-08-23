fac7f315b8cff1acda02ee85ffc86d15c568cad8
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-08-22 20:42:56 -0500

    Optimize pixel shader.

--- src/shaders/pixelFrag.glsl
+++ src/shaders/pixelFrag.glsl
@@ -12,35 +12,32 @@ vec3 getNormal(int x, int y) {
   return texture2D(tNormal, vUv + vec2(x, y) * resolution.zw).rgb * 2.0 - 1.0;
 }
 
-float depthEdgeIndicator() {
-  float depth = getDepth(0, 0);
-  vec3 normal = getNormal(0, 0);
+float depthEdgeIndicator(float depth, vec3 normal) {
   float diff = 0.0;
+
   diff += clamp(getDepth(1, 0) - depth, 0.0, 1.0);
   diff += clamp(getDepth(-1, 0) - depth, 0.0, 1.0);
   diff += clamp(getDepth(0, 1) - depth, 0.0, 1.0);
   diff += clamp(getDepth(0, -1) - depth, 0.0, 1.0);
-  return floor(smoothstep(0.01, 0.02, diff) * 2.) / 2.0;
+
+  return floor(smoothstep(0.01, 0.02, diff) * 2.0) / 2.0;
 }
 
 float neighborNormalEdgeIndicator(int x, int y, float depth, vec3 normal) {
   float depthDiff = getDepth(x, y) - depth;
+  vec3 neighborNormal = getNormal(x, y);
   
-  // Edge pixels should yield to faces whose normals are closer to the bias normal.
-  vec3 normalEdgeBias = vec3(1.0, 1.0, 1.0);
-  float normalDiff = dot(normal - getNormal(x, y), normalEdgeBias);
+  // Edge pixels should yield to faces who's normals are closer to the bias normal.
+  vec3 normalEdgeBias = vec3(1.0, 1.0, 1.0); 
+  float normalDiff = dot(normal - neighborNormal, normalEdgeBias);
   float normalIndicator = clamp(smoothstep(-0.01, 0.01, normalDiff), 0.0, 1.0);
   
   // Only the shallower pixel should detect the normal edge.
   float depthIndicator = clamp(sign(depthDiff * 0.25 + 0.0025), 0.0, 1.0);
-
-  return distance(normal, getNormal(x, y)) * depthIndicator * normalIndicator;
+  return (1.0 - dot(normal, neighborNormal)) * depthIndicator * normalIndicator;
 }
 
-float normalEdgeIndicator() {
-  float depth = getDepth(0, 0);
-  vec3 normal = getNormal(0, 0);
-  
+float normalEdgeIndicator(float depth, vec3 normal) {
   float indicator = 0.0;
 
   indicator += neighborNormalEdgeIndicator(0, -1, depth, normal);
@@ -56,9 +53,23 @@ void main() {
 
   float depthEdgeStrength = 0.3;
   float normalEdgeStrength = 0.4;
+  float depth = 0.0;
 
-  float dei = depthEdgeIndicator();
-  float nei = normalEdgeIndicator();
+  vec3 normal = vec3(0.0);
+  if (depthEdgeStrength > 0.0 || normalEdgeStrength > 0.0) {
+    depth = getDepth(0, 0);
+    normal = getNormal(0, 0);
+  }
+
+  float dei = 0.0;
+  if (depthEdgeStrength > 0.0) {
+    dei = depthEdgeIndicator(depth, normal);
+  }
+
+  float nei = 0.0; 
+  if (normalEdgeStrength > 0.0) {
+    nei = normalEdgeIndicator(depth, normal);
+  }
 
   float Strength = dei > 0.0 ? (1.0 - depthEdgeStrength * dei) : (1.0 + normalEdgeStrength * nei);
 
