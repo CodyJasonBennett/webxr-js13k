ff366750bd6609a353fa0fde3fd1dfcdb328b5c7
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-08-24 09:30:19 -0500

    Update PostProcessing.js

--- src/managers/PostProcessing.js
+++ src/managers/PostProcessing.js
@@ -174,7 +174,7 @@ class PostProcessing {
       cameras.forEach((camera, index) => {
         const [x, y, width, height] = camera.viewport.toArray();
 
-        this.mesh.translateX(xOffset * (index ? 0.5 : -0.5));
+        this.mesh.translateX(xOffset * (index ? 0.25 : -0.25));
 
         this.renderer.setViewport(x, y, width, height);
         this.renderer.setScissor(x, y, width, height);
