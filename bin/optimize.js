aa0d6c3d683989668626babd14d1e5e0784f4658
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-08-24 06:07:18 -0500

    Use defaults, wait for optimizer.

--- bin/optimize.js
+++ bin/optimize.js
@@ -2,27 +2,22 @@
 
 const { join } = require('path');
 const { readFileSync, writeFileSync } = require('fs');
-const { ArrayBufferPool, defaultSparseSelectors, Packer } = require('roadroller');
+const { Packer } = require('roadroller');
 
 const BUNDLE_PATH = join(process.cwd(), 'dist/main.js');
 const TEMPLATE_PATH = join(process.cwd(), 'dist/index.html');
 
 const data = readFileSync(BUNDLE_PATH, 'utf-8');
 
-const selectors = defaultSparseSelectors();
-const packer = new Packer([{ type: 'js', action: 'eval', data }], {
-  sparseSelectors: selectors,
-  maxMemoryMB: 150,
-  arrayBufferPool: new ArrayBufferPool(),
+const packer = new Packer([{ type: 'js', action: 'eval', data }]);
+
+packer.optimize().then(() => {
+  const { firstLine, secondLine } = packer.makeDecoder();
+
+  writeFileSync(
+    TEMPLATE_PATH,
+    readFileSync(TEMPLATE_PATH, 'utf-8')
+      .replace('<script defer="defer" src="main.js"></script>', '')
+      .replace('<body>', `<body><script>${firstLine}\n${secondLine}</script>`)
+  );
 });
-
-packer.optimize();
-
-const { firstLine, secondLine } = packer.makeDecoder();
-
-writeFileSync(
-  TEMPLATE_PATH,
-  readFileSync(TEMPLATE_PATH, 'utf-8')
-    .replace('<script defer="defer" src="main.js"></script>', '')
-    .replace('<body>', `<body><script>${firstLine}\n${secondLine}</script>`)
-);
